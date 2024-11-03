892e8dc0c3be9476aab086eb30b4937e98296224
uncharted9898 <93306735+uncharted9898@users.noreply.github.com>
2024-11-03 18:04:32 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 ARCH="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -ffast-math"
 extracflags="-D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fvariable-expansion-in-unroller"
 March= "-march=armv8-a+crypto+sha2+crc"
-CORE="-mtune=cortex-a73 -mtune=cortex-a53"
+CORE="-mtune=cortex-a76 -mtune=cortex-a55"
 OPTI="-Ofast -pthread -fopenmp -flto -fstrict-aliasing -ftree-vectorize -funroll-loops -ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic -falign-functions=64 -D_REENTRANT -mllvm -enable-loop-distribute"
 
 ./configure CXXFLAGS="$ARCH $extracflags $March $CORE $OPTI" CFLAGS="$ARCH $extracflags $March $CORE $OPTI" \
