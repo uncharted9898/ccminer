434cb51f7e5b19f6f173519f48c6617ff2a62dd9
uncharted9898 <93306735+uncharted9898@users.noreply.github.com>
2024-11-03 17:30:03 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
+ARCH="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -ffast-math"
+extracflags="-D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -fuse-ld=lld -fvariable-expansion-in-unroller"
+March= "-march=armv8-a+crypto+sha2+crc"
+CORE="-mtune=cortex-a73 -mtune=cortex-a53"
+OPTI="-Ofast -pthread -fopenmp -flto -fstrict-aliasing -ftree-vectorize -funroll-loops -ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic -falign-functions=64 -D_REENTRANT -mllvm -enable-loop-distribute"
+
+./configure CXXFLAGS="$ARCH $extracflags $March $CORE $OPTI" CFLAGS="$ARCH $extracflags $March $CORE $OPTI" \
+CXX=clang++ CC=clang  LDFLAGS="-Wl,-hugetlbfs-align -fuse-ld=lld"
+
+
 # To change the cuda arch, edit Makefile.am and run ./build.sh
 
-extracflags="-D_REENTRANT -falign-functions=16 -fomit-frame-pointer -fpic -pthread -flto -L/usr/local/lib64 -fuse-ld=lld -fvariable-expansion-in-unroller"
-
-
-./configure CXXFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -ffast-math -Ofast -funroll-loops -finline-functions $extracflags -march=armv8-a+crypto -mtune=native -mllvm -enable-loop-distribute" CFLAGS="-Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize -Wl,-hugetlbfs-align -ffast-math -march=armv8-a+crypto -mtune=cortex-a53 -Ofast $extracflags -mllvm -enable-loop-distribute"   CXX=clang++ LDFLAGS="-v -flto  -Wl,-hugetlbfs-align" 
 
